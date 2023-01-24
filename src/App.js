ea8759f0c9dc9b9871c512af2b0d5b9dd0a22ff5
ManasNagaraj <knmanas6@gmail.com>
2023-01-24 08:15:45 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './component/RenderComponent'
+import RenderComponent from './component/RenderComponent';
+function App() {
+  return (
+    <div className="App">
+      <RenderComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/RenderComponent.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+
+export default function RenderComponent() {
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        fetch(`http://dev.wildwestrummy-dev.com:8098/sporti/web/home`).then((response) => response.json())
+            .then((data) => setData(data)).catch((error) => { console.log(error.message) }).finally(() => { setLoading(false) });
+
+    },{})
+
+
+    return (
+        <div className="App">
+            <h1>API Posts</h1>
+            {loading && <div>A moment please...</div>}
+            {error && (
+                <div>{`There is a problem fetching the post data - ${error}`}</div>
+            )}
+            <table>
+                <thead>
+                    <tr>
+                        <th>media_id</th>
+                        <th>type</th>
+                        <th>url</th>
+                    </tr>
+                </thead>
+                <tbody>{data &&
+                    data.banners.map(({media_id,type,url}) => {
+                        return [
+                            <tr>
+                                <td key={url}>{media_id}</td>
+                                <td>{type}</td>
+                                <img src ={url} height="180em" width="320em" ></img>
+                            </tr>
+                        ]
+
+                    })}</tbody>
+
+            </table>
+        </div>
+
+    )
+
+}
